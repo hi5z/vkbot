1f0a0611b520ebd59e9f3ee65a6253831560fbc6
z00k <x@2ch.hk>
2015-02-27 14:46:10 +0200

    Few changes - Curl to File get contents

--- autoupdate.php
+++ autoupdate.php
@@ -1,6 +1,6 @@
 <?
-ignore_user_abort(true);
-set_time_limit(0);
+//ignore_user_abort(true);
+//set_time_limit(0);
 
 // Отображать все ошибки или нет//
 //error_reporting(E_ALL);
@@ -10,63 +10,63 @@ require_once "classes.php";
 require_once "config.php";
 
 // Получаем диалоги //
-$input = curl("https://api.vk.com/method/messages.getDialogs?count=10&access_token=" . $token);
+$input = json_decode(file_get_contents("https://api.vk.com/method/messages.getDialogs?count=10&access_token=" . $token));
 
 // Профиль онлайн, когда скрипт работает //
-curl("https://api.vk.com/method/account.setOnline?access_token=" . $token);
+file_get_contents("https://api.vk.com/method/account.setOnline?access_token=" . $token);
 
 
 $i = 0;
 for ($i = 1; $i <= 10; $i++) {
     // Выводим список последних сообщений // ?>
 <div class="panel panel-default">
-    <div class="panel-heading">Отправил <?=$input['response'][$i]['uid']?> в <?=gmdate("Y-m-d\TH:i:s\Z", $input['response'][$i]['date'])?> <? if ($input['response'][$i]['read_state'] == '0')  {echo '<span class="label label-danger">Не прочитано</span>';} else {echo '<span class="label label-success">Прочитано</span>';} ?> <? if ($input['response'][$i]['out'] == '1') {echo '<span class="label label-primary">Ответ отправлен</span>';}?></div>
+    <div class="panel-heading">Отправил <?=$input->response[$i]->uid?> в <?=gmdate("Y-m-d\TH:i:s\Z", $input->response[$i]->date)?> <? if ($input->response[$i]->read_state == '0')  {echo '<span class="label label-danger">Не прочитано</span>';} else {echo '<span class="label label-success">Прочитано</span>';} ?> <? if ($input->response[$i]->out == '1') {echo '<span class="label label-primary">Ответ отправлен</span>';}?></div>
     <div class="panel-body">
-        <?=$input['response'][$i]['body']?>
+        <?=$input->response[$i]->body?>
     </div>
 </div>
 <?
 
-    if ($input['response'][$i]['out'] == '0') {
+    if ($input->response[$i]->out == '0') {
         // VK USER ID CONST //
-        $uid = $input['response'][$i]['uid'];
+        $uid = $input->response[$i]->uid;
 
         // Проверяем ID в базе //
         $result = $link->query("SELECT * FROM clients WHERE vkid=" . $uid);
         $row = mysqli_fetch_array($result);
 
-        $vkmessage = $input['response'][$i]['body'];
+        $vkmessage = $input->response[$i]->body;
 
-        if ($vkmessage[0] == '/'){
+        /* if ($vkmessage[0] == '/'){
             // Эмулируем прочтение сообщения + набор текста + пауза до отправки //
-            curl("https://api.vk.com/method/messages.markAsRead?access_token=" . $token . "&peer_id=" . $uid);
-            sleep(3);
-            curl("https://api.vk.com/method/messages.setActivity?access_token=" . $token . "&user_id=" . $uid . "&type=typing");
+            file_get_contents("https://api.vk.com/method/messages.markAsRead?access_token=" . $token . "&peer_id=" . $uid);
             sleep(4);
+            file_get_contents("https://api.vk.com/method/messages.setActivity?access_token=" . $token . "&user_id=" . $uid . "&type=typing");
+            sleep(5);
 
             $mes = cmd(substr($vkmessage, 1));
             $res = file_get_contents("https://api.vk.com/method/messages.send?access_token=" . $token . "&message=" . urlencode($mes) . "&uid=" . $uid);
         }
-        elseif ($row['vkid'] == $uid) {
+        else*/if ($row['vkid'] == $uid) {
             // Эмулируем прочтение сообщения + набор текста + пауза до отправки //
-            curl("https://api.vk.com/method/messages.markAsRead?access_token=" . $token . "&peer_id=" . $uid);
-            sleep(3);
-            curl("https://api.vk.com/method/messages.setActivity?access_token=" . $token . "&user_id=" . $uid . "&type=typing");
+            file_get_contents("https://api.vk.com/method/messages.markAsRead?access_token=" . $token . "&peer_id=" . $uid);
             sleep(4);
+            file_get_contents("https://api.vk.com/method/messages.setActivity?access_token=" . $token . "&user_id=" . $uid . "&type=typing");
+            sleep(5);
 
             // Получаем ответ на сообщение //
             $mes = file_get_contents('http://bot.mew.su/sp.php?session=' . $row['chatid'] . '&text=' . urlencode($vkmessage));
 
             // Отсылаем сообщение //
             $res = file_get_contents("https://api.vk.com/method/messages.send?access_token=" . $token . "&message=" . urlencode($mes) . "&uid=" . $uid);
-
+            sleep(1);
         } else {
 
             // Если мы еще не общались с этим профилем - добавляем его в базу и отсылаем боту имя //
             $vkprofileinfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?name_case=nom&fields=sex&user_ids=" . $uid), true);
-            $firstname = $vkprofileinfo['response'][0]['first_name'];
-            $secondname = $vkprofileinfo['response'][0]['last_name'];
-            $sex = $vkprofileinfo['response'][0]['sex'];
+            $firstname = $vkprofileinfo->response[0]->first_name;
+            $secondname = $vkprofileinfo->response[0]->last_name;
+            $sex = $vkprofileinfo->response[0]->sex;
             $chatid = file_get_contents('http://bot.mew.su/showmeid.php?id=' . $uid);
 
 
@@ -79,13 +79,13 @@ for ($i = 1; $i <= 10; $i++) {
             $row2 = mysqli_fetch_array($result2);
 
             // Устанавливаем имя //
-            curl('http://bot.mew.su/sp.php?session=' . $row2['chatid'] . '&text=' . urlencode('!botsetname ' . $row2['firstname']));
+            file_get_contents('http://bot.mew.su/sp.php?session=' . $row2['chatid'] . '&text=' . urlencode('!botsetname ' . $row2['firstname']));
 
             // Устанавливаем пол //
             if ($row2['sex'] == '2'){
-                curl('http://bot.mew.su/sp.php?session=' . $row2['chatid'] . '&text=' . urlencode('я мальчик'));
+                file_get_contents('http://bot.mew.su/sp.php?session=' . $row2['chatid'] . '&text=' . urlencode('я мальчик'));
             } elseif ($row2['sex'] == '1') {
-                curl('http://bot.mew.su/sp.php?session=' . $row2['chatid'] . '&text=' . urlencode('я девочка'));
+                file_get_contents('http://bot.mew.su/sp.php?session=' . $row2['chatid'] . '&text=' . urlencode('я девочка'));
             }
         }
     }
